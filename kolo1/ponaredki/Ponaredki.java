4bf0ab89b369538efccccf4869bed1320f49f85f
Nejc Jezeršek <njezersek@gmail.com>
2020-04-16 17:49:44 +0200

    ponaredki

--- kolo1/ponaredki/Ponaredki.java
+++ kolo1/ponaredki/Ponaredki.java
@@ -9,7 +9,7 @@ public class Ponaredki {
         for(int i=0; i<n; i++){
             int orig = sc.nextInt();
             int fake = sc.nextInt();
-            if(p(1, 0, orig, fake)){
+            if(p(orig, fake)){
                 System.out.println("DA");
             }else{
                 System.out.println("NE");
@@ -19,12 +19,20 @@ public class Ponaredki {
         sc.close();
     }
 
-    public static boolean p(int orig, int fake, int targetOrig, int targetFake){
+    public static boolean p(int orig, int fake){
+        fake -= orig-1;
+        if(fake < 0)return false;
+        if(fake == 0)return true;
+        if(orig > 1 && fake % 2 == 0)return true;
+        return false;
+    }
+
+    /*public static boolean p(int orig, int fake, int targetOrig, int targetFake){
         if(orig == targetOrig && fake == targetFake)return true;
         if(orig > targetOrig || fake > targetFake)return false;
         boolean ok = false;
         if(p(orig+1, fake+1, targetOrig, targetFake))ok = true;
         if(fake > 0)if(p(orig, fake+2, targetOrig, targetFake))ok = true;
         return ok;
-    }
+    }*/
 }
